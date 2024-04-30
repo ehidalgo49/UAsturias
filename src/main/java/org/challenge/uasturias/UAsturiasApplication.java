9a83104d3b0078d775ae881d56c5239a43c7b63b
ehidalgo49 <ehidalgo49@hotmail.com>
2024-04-29 23:41:08 -0500

    initial commit

---
+++ src/main/java/org/challenge/uasturias/UAsturiasApplication.java
@@ -0,0 +1,13 @@
+package org.challenge.uasturias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UAsturiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UAsturiasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/challenge/uasturias/UAsturiasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.challenge.uasturias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UAsturiasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
