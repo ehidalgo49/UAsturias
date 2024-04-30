d031def0a4853616041f3cea2504a8c23a6c6f6d
ehidalgo49 <ehidalgo49@hotmail.com>
2024-04-30 00:47:28 -0500

    Create test main options

--- src/main/java/org/challenge/uasturias/UAsturiasApplication.java
+++ src/main/java/org/challenge/uasturias/UAsturiasApplication.java
@@ -1,5 +1,8 @@
 package org.challenge.uasturias;
 
+import org.challenge.fizzbuzz.FizzBuzz;
+import org.challenge.maxadjsum.MaxAdjSum;
+import org.challenge.notificationservice.Solution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UAsturiasApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UAsturiasApplication.class, args);
+        FizzBuzz fizz = new FizzBuzz();
+        MaxAdjSum max = new MaxAdjSum();
+        Solution sol = new Solution();
+
+        //fizz.init(); //Ejercicio 1
+        //max.init(new int[]{4, 8, -1, -3, 7, 8}); //Ejercicio 2
+        sol.init(); //Ejercicio 3
+
+        //SpringApplication.run(UAsturiasApplication.class, args);
     }
 
 }
